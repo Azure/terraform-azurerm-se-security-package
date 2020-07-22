90bda1d1d7d8ac8e6c6c651d4ecf585ca7b1d241
jjcollinge <jonathancollinge@live.com>
2020-07-22 14:58:07 +0100

    remove module_depends_on var

--- variables.tf
+++ variables.tf
@@ -70,7 +70,3 @@ variable "enabled_for_template_deployment" {
   description = "Enable Key Vault to be used in ARM templates deployments"
   default     = false
 }
-
-variable "module_depends_on" {
-  default = [""]
-}
